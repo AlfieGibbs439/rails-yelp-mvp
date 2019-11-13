45895468b47aa36389b2f93c4d10ecf4469a0be2
Alfie Gibbs <alfie.gibbs@outlook.com>
2019-11-13 14:50:43 +0000

    restaurant and reviews app complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  restaurants = Restaurant.create!([
+    { name: "MacDonalds", address: "Farringdon", phone_number: "02289712944", category: "chinese"},
+    { name: "Pizza Express", address: "London", phone_number: "07189712944", category: "italian"},
+    { name: "Itsu", address: "Chelsea", phone_number: "04189712944", category: "japanese"},
+    { name: "Wagamamas", address: "Leeds", phone_number: "08189712944", category: "chinese"},
+    { name: "Burger King", address: "Paris", phone_number: "06189712944", category: "belgian"},
+    { name: "Pret", address: "Berlin", phone_number: "04189712944", category: "french"}
+    ])
